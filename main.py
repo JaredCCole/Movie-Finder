4d67eb23a8b370e267d444fee9ddc9a9ecb9d2d6
Jared Cole <69369570+JaredCCole@users.noreply.github.com>
2022-01-18 00:21:25 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import requests
+import pprint
+import random
+from APIConnector import IMDbConnector
+
+main = IMDbConnector()#change this line for different search parameters found in APIConnector.py
+
+mainCount = main.count[7:]#gets the count not including the '&count='
+
+randomNum = random.randrange(int(mainCount))#picks a random index from selected items of a certain count
+
+selected = IMDbConnector.getJSON(main.getURL())["results"][randomNum]#selects the item
+
+
+print(f"Title: {selected['title']} {selected['description']} {selected['contentRating']} IMDb Rating: {selected['imDbRating']}\n\n{selected['plot']}\n")
+
+iD = selected["id"]
+
+youTubeTrailer = "https://imdb-api.com/API/YouTubeTrailer/k_2y7947d4/" + iD
+
+print(IMDbConnector.getJSON(youTubeTrailer)["videoUrl"])
+print()
